3c0d8ddc77b8f03204468238660ebb77ebb24964
André Schulz <git@anschu.dev>
2023-10-29 01:56:32 +0200

    update

---
+++ .config/zsh/.zsh_plugins.zsh
@@ -0,0 +1,18 @@
+fpath+=( /Users/anschu/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zap-zsh-SLASH-supercharge )
+source /Users/anschu/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zap-zsh-SLASH-supercharge/supercharge.plugin.zsh
+fpath+=( /Users/anschu/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zap-zsh-SLASH-completions )
+source /Users/anschu/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zap-zsh-SLASH-completions/completions.plugin.zsh
+fpath+=( /Users/anschu/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zap-zsh-SLASH-vim )
+source /Users/anschu/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zap-zsh-SLASH-vim/vim.plugin.zsh
+fpath+=( /Users/anschu/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zap-zsh-SLASH-fzf )
+source /Users/anschu/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zap-zsh-SLASH-fzf/fzf.plugin.zsh
+fpath+=( /Users/anschu/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-sindresorhus-SLASH-pure )
+source /Users/anschu/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-sindresorhus-SLASH-pure/pure.plugin.zsh
+fpath+=( /Users/anschu/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-wintermi-SLASH-zsh-brew )
+source /Users/anschu/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-wintermi-SLASH-zsh-brew/zsh-brew.plugin.zsh
+fpath+=( /Users/anschu/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-syntax-highlighting )
+source /Users/anschu/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-syntax-highlighting/zsh-syntax-highlighting.plugin.zsh
+fpath+=( /Users/anschu/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions )
+source /Users/anschu/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions/zsh-autosuggestions.plugin.zsh
+fpath+=( /Users/anschu/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-history-substring-search )
+source /Users/anschu/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-history-substring-search/zsh-history-substring-search.plugin.zsh
